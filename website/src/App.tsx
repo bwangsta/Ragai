aceea2a516991b241da3c99f234a4f79f859a0a4
Brian Wang <bwbrianwang1018@gmail.com>
2023-10-17 00:37:34 -0700

    Initial website setup

---
+++ website/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Vite + React
+      </h1>
+    </>
+  )
+}
+
+export default App
