14759a83bf393d3118e00010affda4cd72d4b5a9
 <jonathan.dao@protonmail.com>
2023-11-24 16:45:12 +0100

    Removed function to manage index

--- models/src/embedding/search.py
+++ models/src/embedding/search.py
@@ -4,6 +4,3 @@ def get_most_similar_items(image_url, index, k, namespace):
     image_query = get_image(image_url)
     query_embedding = get_single_image_embedding(image_query).tolist()
     return index.query(query_embedding, top_k=k, include_metadata=True, namespace=namespace)    
-
-def delete_with_id(id, index, namespace):
-    delete_response = index.delete(ids=[id], namespace=namespace)
