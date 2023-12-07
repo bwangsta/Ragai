46d1a045d0d2884884b51faf72ec5f30377e0f50
Brian Wang <60533686+bwangsta@users.noreply.github.com>
2023-12-07 11:58:07 -0800

    Build filter modal

--- mobile/src/screens/InventoryScreen.tsx
+++ mobile/src/screens/InventoryScreen.tsx
@@ -1,7 +1,16 @@
 import { useState, useCallback } from "react"
-import { Image, StyleSheet, Text, View, FlatList } from "react-native"
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+} from "react-native"
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs"
 import { useFocusEffect } from "@react-navigation/native"
+import { Ionicons } from "@expo/vector-icons"
 import SafeArea from "../components/SafeArea"
 import { getData } from "../services/api"
 import { colors } from "../styles/colors"
@@ -16,6 +25,9 @@ type Item = {
 export default function InventoryScreen() {
   const tabBarHeight = useBottomTabBarHeight()
   const [itemsData, setItemsData] = useState<Item[]>([])
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const allTags = itemsData.flatMap((item) => item.tags)
+  const tagsCounter: { [key: string]: number } = {}
 
   useFocusEffect(
     useCallback(() => {
@@ -25,11 +37,74 @@ export default function InventoryScreen() {
     }, [])
   )
 
+  allTags.map((tag) => {
+    if (tag in tagsCounter) {
+      tagsCounter[tag] += 1
+    } else {
+      tagsCounter[tag] = 1
+    }
+  })
+
+  function openModal() {
+    setIsModalVisible(true)
+  }
+
+  function closeModal() {
+    setIsModalVisible(false)
+  }
+
   return (
     <SafeArea
       insets="top left right"
       style={{ backgroundColor: colors.white, marginBottom: tabBarHeight }}
     >
+      <TouchableOpacity style={{ alignSelf: "flex-end" }} onPress={openModal}>
+        <Ionicons name="filter" size={24} />
+      </TouchableOpacity>
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        onRequestClose={closeModal}
+      >
+        <SafeArea>
+          <TouchableOpacity onPress={closeModal}>
+            <Ionicons name="close-sharp" size={24} />
+          </TouchableOpacity>
+          <View style={{ flex: 1 }}>
+            <Text>Tags</Text>
+            <View style={{ flexDirection: "row", flexWrap: "wrap", gap: 4 }}>
+              {Object.entries(tagsCounter).map(([tag, count]) => (
+                <TouchableOpacity
+                  key={tag}
+                  style={{
+                    flexDirection: "row",
+                    gap: 4,
+                    backgroundColor: "gray",
+                    paddingVertical: 4,
+                    paddingHorizontal: 8,
+                    borderRadius: 16,
+                  }}
+                >
+                  <View style={{ flexDirection: "row", gap: 4 }}>
+                    <Text style={{ color: colors.white }}>{tag}</Text>
+                    <Text style={{ color: colors.white }}>{count}</Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              backgroundColor: "gray",
+              paddingVertical: 16,
+            }}
+            onPress={closeModal}
+          >
+            <Text>Apply Filters</Text>
+          </TouchableOpacity>
+        </SafeArea>
+      </Modal>
       <FlatList
         data={itemsData}
         extraData={itemsData}
